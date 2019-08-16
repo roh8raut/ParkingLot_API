17b69191b8b280400e525f8f28257f355a7b9d06
roh8raut <roh8raut@gmail.com>
2019-08-16 12:15:34 +0530

    setting up to push to Heroku

--- app/src/server.js
+++ app/src/server.js
@@ -2,10 +2,15 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import router from './routes/route';
 import mongoose from 'mongoose';
+require('dotenv').config();
 var cors = require('cors')
 
+// const url =  'mongodb://localhost:27017/parkinglot';
+
 const app = express();
-const port = 5000
+const port = process.env.PORT || 5000;
+
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/parkinglot', {useNewUrlParser: true});
 
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
--- app/src/services/parkinglot.services.js
+++ app/src/services/parkinglot.services.js
@@ -1,6 +1,4 @@
-import mongoose from 'mongoose';
-import parkingLotData from '../models/parkinglot.model';
-const url =  'mongodb://localhost:27017/parkinglot';
+import parkingLotData from "../models/parkinglot.model";
 
 const slotsArray = [];
 let dataToReturn;
@@ -10,36 +8,44 @@ for (let i = 1; i < 21; i++) {
   slotsArray.push(i);
 }
 
-mongoose.connect(url, {useNewUrlParser: true});
-
-
-export const getSlotNumber = async (regNum) => {
-    await parkingLotData.findOne({},{"parkingDetails": {$elemMatch : {regNum: regNum }}})
-          .then((data) => {
-              dataToReturn = (data && data.parkingDetails && data.parkingDetails.length > 0) ? {"msg": "Your car is parked in slot: ",slotAlloted: data.parkingDetails[0].slotAlloted} : {"msg": "No Car is Parked with that registration number:("};
-            });
-            return dataToReturn;
-}
+export const getSlotNumber = async regNum => {
+  await parkingLotData
+    .findOne({}, { parkingDetails: { $elemMatch: { regNum: regNum } } })
+    .then(data => {
+      dataToReturn =
+        data && data.parkingDetails && data.parkingDetails.length > 0
+          ? {
+              msg: "Your car is parked in slot: ",
+              slotAlloted: data.parkingDetails[0].slotAlloted
+            }
+          : { msg: "No Car is Parked with that registration number:(" };
+    });
+  return dataToReturn;
+};
 // await parkingLotData.findOneAndUpdate({ "_id" : "5d30af93df497513e52d0a30"},{$push: {parkingDetails: {dataObj}}})
 export const getAllAvailableSlots = async () => {
-  
-  return (slotsArray.length > 0) ? {"slots": slotsArray} : {"msg": "All Slots are booked"};
-
-}
- export const postDataToTable = async (req) => {
+  return slotsArray.length > 0
+    ? { slots: slotsArray }
+    : { msg: "All Slots are booked" };
+};
+export const postDataToTable = async req => {
   // let ds = new parkingLotData({})
-  await parkingLotData.find({"parkingDetails": {$elemMatch: {"regNum": req.regNum}}})
-                      .then(async (data) => {
-                        if(data.length !== 0) {
-                          dataToReturn = {"msg": "We are sorry to inform that your entry already exists."}
-                        } else {
-                          let isSlotAlloted = await getAvailableSlot(req.regNum);
-                          dataToReturn =  isSlotAlloted ? {"msg": "Your car is parked in slot: ","slotAlloted": isSlotAlloted}:{"msg": "All slots are booked"}
-                        }
- 
-                      })
-                      return dataToReturn;
-}
+  await parkingLotData
+    .find({ parkingDetails: { $elemMatch: { regNum: req.regNum } } })
+    .then(async data => {
+      if (data.length !== 0) {
+        dataToReturn = {
+          msg: "We are sorry to inform that your entry already exists."
+        };
+      } else {
+        let isSlotAlloted = await getAvailableSlot(req.regNum);
+        dataToReturn = isSlotAlloted
+          ? { msg: "Your car is parked in slot: ", slotAlloted: isSlotAlloted }
+          : { msg: "All slots are booked" };
+      }
+    });
+  return dataToReturn;
+};
 
 // export const updateDataToTable = async () => {
 //   await parkingLotData.findOneAndUpdate()
@@ -49,37 +55,58 @@ export const getAllAvailableSlots = async () => {
 //           return dataToReturn;
 // }
 
-export const deleteDataFromTable = async (regNum) => {
-  
-  await parkingLotData.findOne({},{"parkingDetails": {$elemMatch: {regNum: regNum}}})
-          .then(async (data) => {
-              dataToReturn = (data && data.parkingDetails && data.parkingDetails.length > 0) ? addSlotToSlotArr(data) : {"msg": "No Car is Parked with that registration number:("};
-          });
-          return dataToReturn;
-}
+export const deleteDataFromTable = async regNum => {
+  await parkingLotData
+    .findOne({}, { parkingDetails: { $elemMatch: { regNum: regNum } } })
+    .then(async data => {
+      dataToReturn =
+        data && data.parkingDetails && data.parkingDetails.length > 0
+          ? addSlotToSlotArr(data)
+          : { msg: "No Car is Parked with that registration number:(" };
+    });
+  return dataToReturn;
+};
 
 // > db.parkinglotdatas.findOne({},{"parkingDetails": {$elemMatch: {regNum: "1234"}}})
 
-const getAvailableSlot  = async (regNum) => {
-      if(slotsArray.length > 0){
-        let slotAlloted = parseInt(slotsArray.splice(0,1));
-          await parkingLotData.findOneAndUpdate({$push: {"parkingDetails": {regNum: regNum, slotAlloted: slotAlloted, bookingDateAndTime: new Date()}}})
-          return slotAlloted;
-      } else {
-        return "";
+const getAvailableSlot = async regNum => {
+  if (slotsArray.length > 0) {
+    let slotAlloted = parseInt(slotsArray.splice(0, 1));
+    await parkingLotData.findOneAndUpdate({
+      $push: {
+        parkingDetails: {
+          regNum: regNum,
+          slotAlloted: slotAlloted,
+          bookingDateAndTime: new Date()
+        }
       }
-}
+    });
+    return slotAlloted;
+  } else {
+    return "";
+  }
+};
 
-const addSlotToSlotArr = async (data) => {
-  const dataObj = {slotToBeAdded: data.parkingDetails[0].slotAlloted, bookingDate: data.parkingDetails[0].bookingDateAndTime, regNum: data.parkingDetails[0].regNum};
+const addSlotToSlotArr = async data => {
+  const dataObj = {
+    slotToBeAdded: data.parkingDetails[0].slotAlloted,
+    bookingDate: data.parkingDetails[0].bookingDateAndTime,
+    regNum: data.parkingDetails[0].regNum
+  };
   const amountToBePaid = calculateAmountToPaid(dataObj.bookingDate);
-    await parkingLotData.update({},{$pull: {"parkingDetails": {regNum: dataObj.regNum}}})
-        slotsArray.push(dataObj.slotToBeAdded);
-        return {"msg": "Your car is moved from parking space.","amountToBePaid":`Amount to be paid ${amountToBePaid}₹`};
-}
+  await parkingLotData.update(
+    {},
+    { $pull: { parkingDetails: { regNum: dataObj.regNum } } }
+  );
+  slotsArray.push(dataObj.slotToBeAdded);
+  return {
+    msg: "Your car is moved from parking space.",
+    amountToBePaid: `Amount to be paid ${amountToBePaid}₹`
+  };
+};
 
-const calculateAmountToPaid = (bookingDate) => {
-    let currentDate = new Date();
-    const diff = currentDate.getTime() - bookingDate.getTime();
-    return (Math.floor(diff/60000) === 0) ? 1 : Math.floor(diff/60000);
-}
+const calculateAmountToPaid = bookingDate => {
+  let currentDate = new Date();
+  const diff = currentDate.getTime() - bookingDate.getTime();
+  return Math.floor(diff / 60000) === 0 ? 1 : Math.floor(diff / 60000);
+};
