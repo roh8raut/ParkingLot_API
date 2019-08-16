4adc580dcabd17f8fff914010f5861f237870493
roh8raut <roh8raut@gmail.com>
2019-08-16 17:59:34 +0530

    setting up to push to Heroku

--- app/src/server.js
+++ app/src/server.js
@@ -2,14 +2,16 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import router from './routes/route';
 import mongoose from 'mongoose';
+
 require('dotenv').config();
 var cors = require('cors')
 
 // const url =  'mongodb://localhost:27017/parkinglot';
 
 const app = express();
-const port = process.env.PORT || 5000;
-
+// const port = process.env.PORT || 5000;
+const port = 5000;
+console.log("env>>", process.env.PORT);
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/parkinglot', {useNewUrlParser: true});
 
 app.use(cors());
